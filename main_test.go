9dfeec921ba6f6625c48e1e5ea03f4f70859f859
vinicus rodrigues <vinitecnico@gmail.com>
2020-09-20 16:13:55 -0300

    add test get person

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -29,7 +30,37 @@ func TestGetPeopleShouldReturnSuccess(t *testing.T) {
 	// Check the response body is what we expect.
 	expected := `[{"id":"1","firstname":"John","lastname":"Doe","address":{"city":"City X","state":"State X"}}]`
 	result := rr.Body.String()
-	if rr.Body == nil {
+	result = strings.TrimSuffix(result, "\n")
+	if result != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", result, expected)
+	}
+}
+
+func TestGetPersonShouldReturnSuccess(t *testing.T) {
+	req, err := http.NewRequest("GET", "/contato/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetPerson)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `{}`
+	result := rr.Body.String()
+	result = strings.TrimSuffix(result, "\n")
+	if result != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", result, expected)
 	}
 }
